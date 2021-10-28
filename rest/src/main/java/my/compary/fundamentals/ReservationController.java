e1e01ed166d7a1d8a7f40fd935e35c302fc13978
Otavio Santana <otaviopolianasantana@gmail.com>
2021-10-28 12:58:41 +0100

    Create reservation controller
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- rest/src/main/java/my/compary/fundamentals/ReservationController.java
+++ rest/src/main/java/my/compary/fundamentals/ReservationController.java
@@ -1,28 +1,47 @@
 package my.compary.fundamentals;
 
 
+import com.github.javafaker.Faker;
+
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutorService;
 
 @RequestScoped
 @Path("reservations")
 public class ReservationController {
 
+    @Inject
     private ExecutorService executorService;
+
+    @Inject
+    private FakerService service;
 
     @Path("/cities")
     @GET
     public void getReservation(@QueryParam("from") String from,
                                @QueryParam("to") String to,
-                               @QueryParam("start") LocalDate start,
                                @Suspended AsyncResponse async) {
+        executorService.execute(() -> {
+            List<String> cities = service.getCities();
+            async.resume(cities);
+        });
+    }
 
-
+    @GET
+    @Path("countries")
+    public CompletionStage<List<String>> getCountries() {
+        CompletableFuture<List<String>> future = CompletableFuture
+                .supplyAsync(() -> service.getCountries());
+        return future;
     }
 }
