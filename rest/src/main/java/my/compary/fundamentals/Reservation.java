18fed68b82d56cf20bde91d2bdaf2eb0c587f41a
Otavio Santana <otaviopolianasantana@gmail.com>
2021-10-28 12:39:41 +0100

    creates reservation class
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- rest/src/main/java/my/compary/fundamentals/Reservation.java
+++ rest/src/main/java/my/compary/fundamentals/Reservation.java
@@ -1,2 +1,28 @@
-package my.compary.fundamentals;public class Reservation {
+package my.compary.fundamentals;
+
+import my.compary.fundamentals.infra.FieldPropertyVisibilityStrategy;
+
+import javax.json.bind.annotation.JsonbVisibility;
+import java.time.LocalDate;
+
+@JsonbVisibility(FieldPropertyVisibilityStrategy.class)
+public class Reservation {
+
+    private String to;
+
+    private String from;
+
+    private LocalDate start;
+
+    public String getTo() {
+        return to;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public LocalDate getStart() {
+        return start;
+    }
 }
--- rest/src/main/java/my/compary/fundamentals/ReservationController.java
+++ rest/src/main/java/my/compary/fundamentals/ReservationController.java
@@ -1,2 +1,28 @@
-package my.compary.fundamentals;public class ReservationController {
+package my.compary.fundamentals;
+
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
+import java.time.LocalDate;
+import java.util.concurrent.ExecutorService;
+
+@RequestScoped
+@Path("reservations")
+public class ReservationController {
+
+    private ExecutorService executorService;
+
+    @Path("/cities")
+    @GET
+    public void getReservation(@QueryParam("from") String from,
+                               @QueryParam("to") String to,
+                               @QueryParam("start") LocalDate start,
+                               @Suspended AsyncResponse async) {
+
+
+    }
 }
