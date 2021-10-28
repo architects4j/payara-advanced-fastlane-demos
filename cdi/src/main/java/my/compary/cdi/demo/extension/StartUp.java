28b590c2909c2e35f65408641c2eebfaad12bfcf
Otavio Santana <otaviopolianasantana@gmail.com>
2021-10-28 11:11:39 +0100

    updates startup bean extension

--- cdi/src/main/java/my/compary/cdi/demo/extension/StartUp.java
+++ cdi/src/main/java/my/compary/cdi/demo/extension/StartUp.java
@@ -1,4 +1,17 @@
 package my.compary.cdi.demo.extension;
 
+import javax.inject.Qualifier;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Qualifier
+@Retention(RUNTIME)
+@Target({TYPE, METHOD, FIELD, PARAMETER})
 public @interface StartUp {
 }
