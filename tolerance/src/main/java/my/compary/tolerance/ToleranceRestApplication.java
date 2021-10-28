619851ac9ae029569f54b4c3fc705e690869c6c0
Otavio Santana <otaviopolianasantana@gmail.com>
2021-10-28 20:56:12 +0100

    create tolerance project
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ tolerance/src/main/java/my/compary/tolerance/ToleranceRestApplication.java
@@ -0,0 +1,13 @@
+package my.compary.tolerance;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+@ApplicationScoped
+public class ToleranceRestApplication extends Application {
+}
