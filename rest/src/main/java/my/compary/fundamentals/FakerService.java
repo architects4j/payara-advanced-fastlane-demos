8f1df2413e53a6656d1c3aa093fa48e88c9eb846
Otavio Santana <otaviopolianasantana@gmail.com>
2021-10-28 12:58:28 +0100

    define a fake service to create countries and cities
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- rest/src/main/java/my/compary/fundamentals/FakerService.java
+++ rest/src/main/java/my/compary/fundamentals/FakerService.java
@@ -1,2 +1,48 @@
-package my.compary.fundamentals;public class FakerService {
+package my.compary.fundamentals;
+
+
+import com.github.javafaker.Address;
+import com.github.javafaker.Country;
+import com.github.javafaker.Faker;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@ApplicationScoped
+public class FakerService {
+
+    @Inject
+    private Faker faker;
+
+    public List<String> getCities() {
+        List<String> cities = new ArrayList<>();
+        Address address = faker.address();
+        for (int index = 0; index < 20; index++) {
+            cities.add(address.cityName());
+        }
+        waitTwoSeconds();
+        return cities;
+    }
+
+    public List<String> getCountries() {
+        List<String> countries = new ArrayList<>();
+        Country country = faker.country();
+        for (int index = 0; index < 20; index++) {
+            countries.add(country.name());
+        }
+        waitTwoSeconds();
+        return countries;
+    }
+
+    private void waitTwoSeconds() {
+        try {
+            TimeUnit.SECONDS.sleep(2L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
