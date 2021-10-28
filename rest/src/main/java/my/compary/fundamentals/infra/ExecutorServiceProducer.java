ecbd21c305855e42418af18f49e109aa33c99a47
Otavio Santana <otaviopolianasantana@gmail.com>
2021-10-28 12:39:26 +0100

    Create Executor producer
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- rest/src/main/java/my/compary/fundamentals/infra/ExecutorServiceProducer.java
+++ rest/src/main/java/my/compary/fundamentals/infra/ExecutorServiceProducer.java
@@ -1,2 +1,17 @@
-package my.compary.fundamentals.infra;public class ExecutorServiceProducer {
+package my.compary.fundamentals.infra;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@ApplicationScoped
+class ExecutorServiceProducer {
+
+    private static final int THREADS = 10;
+
+    @Produces
+    ExecutorService getExecutorService() {
+        return Executors.newFixedThreadPool(THREADS);
+    }
 }
