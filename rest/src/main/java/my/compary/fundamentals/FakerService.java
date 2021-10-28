86db9e95e75224d9f87e2d5f51879ad342826c36
Otavio Santana <otaviopolianasantana@gmail.com>
2021-10-28 13:00:02 +0100

    adds logs in the faker service

--- rest/src/main/java/my/compary/fundamentals/FakerService.java
+++ rest/src/main/java/my/compary/fundamentals/FakerService.java
@@ -10,14 +10,18 @@ import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
 
 @ApplicationScoped
 public class FakerService {
+
+    private static Logger LOGGER = Logger.getLogger(FakerService.class.getName());
 
     @Inject
     private Faker faker;
 
     public List<String> getCities() {
+        LOGGER.info("Starting the cities service");
         List<String> cities = new ArrayList<>();
         Address address = faker.address();
         for (int index = 0; index < 20; index++) {
@@ -28,6 +32,7 @@ public class FakerService {
     }
 
     public List<String> getCountries() {
+        LOGGER.info("Starting the countries service");
         List<String> countries = new ArrayList<>();
         Country country = faker.country();
         for (int index = 0; index < 20; index++) {
@@ -38,6 +43,7 @@ public class FakerService {
     }
 
     private void waitTwoSeconds() {
+        LOGGER.info("We'll wait for two seconds");
         try {
             TimeUnit.SECONDS.sleep(2L);
         } catch (InterruptedException e) {
