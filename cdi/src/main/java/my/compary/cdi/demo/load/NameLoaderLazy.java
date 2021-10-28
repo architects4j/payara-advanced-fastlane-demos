fbc15ef8fd415cb5d5866c30f59b3b9c73aacee7
Otavio Santana <otaviopolianasantana@gmail.com>
2021-10-28 11:39:37 +0100

    Update sample code
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- cdi/src/main/java/my/compary/cdi/demo/load/NameLoaderLazy.java
+++ cdi/src/main/java/my/compary/cdi/demo/load/NameLoaderLazy.java
@@ -1,30 +1,37 @@
 package my.compary.cdi.demo.load;
 
-import com.github.javafaker.DragonBall;
 import com.github.javafaker.Faker;
-import my.compary.cdi.demo.extension.StartUp;
+import com.github.javafaker.Pokemon;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 import java.util.logging.Logger;
 
 @ApplicationScoped
-@StartUp
-public class NameLoaderLazy {
+public class NameLoaderLazy implements Supplier<Set<String>> {
 
     private static final Logger LOGGER = Logger.getLogger(NameLoaderLazy.class.getName());
 
     private final Set<String> names = new HashSet<>();
 
     @PostConstruct
-    public void onStartup() {
-        LOGGER.info("it will load eagerly");
+    void onStartup() throws InterruptedException {
+        LOGGER.info("it will load lazily");
+        TimeUnit.SECONDS.sleep(2L);
         Faker faker = new Faker();
-        DragonBall dragonBall = faker.dragonBall();
+        Pokemon pokemon = faker.pokemon();
         for (int index = 0; index < 15; index++) {
-            this.names.add(dragonBall.character());
+            this.names.add(pokemon.name());
         }
+    }
+
+    @Override
+    public Set<String> get() {
+        return Collections.unmodifiableSet(names);
     }
 }
