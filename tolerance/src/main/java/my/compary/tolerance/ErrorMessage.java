f034dc4d6b6d3636c67970d5c26f57523e3d51ad
Otavio Santana <otaviopolianasantana@gmail.com>
2021-10-28 21:01:49 +0100

    Create error message
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ tolerance/src/main/java/my/compary/tolerance/ErrorMessage.java
@@ -0,0 +1,11 @@
+package my.compary.tolerance;
+
+import org.eclipse.microprofile.faulttolerance.ExecutionContext;
+import org.eclipse.microprofile.faulttolerance.FallbackHandler;
+
+public class ErrorMessage implements FallbackHandler<String> {
+    @Override
+    public String handle(ExecutionContext context) {
+        return "Fallback answer due to timeout";
+    }
+}
