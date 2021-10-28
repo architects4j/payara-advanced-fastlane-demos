a2e599009709cff1f33880d44e0dc469be52f059
Otavio Santana <otaviopolianasantana@gmail.com>
2021-10-28 13:57:47 +0100

    fixes faker producer with @Produces annotation from CDI
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- rest/src/main/java/my/compary/fundamentals/infra/FakerProducer.java
+++ rest/src/main/java/my/compary/fundamentals/infra/FakerProducer.java
@@ -3,11 +3,13 @@ package my.compary.fundamentals.infra;
 import com.github.javafaker.Faker;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
 
 @ApplicationScoped
 class FakerProducer {
 
     @ApplicationScoped
+    @Produces
     Faker getFaker() {
         return new Faker();
     }
