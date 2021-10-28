8a82ceef11a6c504727868fbfe94d5dd5fa7572b
Otavio Santana <otaviopolianasantana@gmail.com>
2021-10-28 21:30:01 +0100

    Add logger message
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- tolerance/src/main/java/my/compary/tolerance/ErrorMessage.java
+++ tolerance/src/main/java/my/compary/tolerance/ErrorMessage.java
@@ -3,9 +3,17 @@ package my.compary.tolerance;
 import org.eclipse.microprofile.faulttolerance.ExecutionContext;
 import org.eclipse.microprofile.faulttolerance.FallbackHandler;
 
+import java.util.logging.Logger;
+
 public class ErrorMessage implements FallbackHandler<String> {
+
+    private static final Logger LOGGER = Logger.getLogger(ErrorMessage.class.getName());
+
     @Override
     public String handle(ExecutionContext context) {
+        String message = String.format("The execution method: %s error message %s error message %s",
+                context.getMethod().getName(), context.getFailure().getMessage());
+        LOGGER.info(message);
         return "Fallback answer due to timeout";
     }
 }
