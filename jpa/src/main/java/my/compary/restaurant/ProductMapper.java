3ef5f4a45c5d61e4ee8cb9cbd447b6481c15cc66
Otavio Santana <otaviopolianasantana@gmail.com>
2021-10-29 18:27:05 +0100

    Create ProductMapper as interface
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jpa/src/main/java/my/compary/restaurant/ProductMapper.java
@@ -0,0 +1,53 @@
+package my.compary.restaurant;
+
+import org.javamoney.moneta.Money;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import javax.money.MonetaryAmount;
+import java.time.LocalDate;
+
+@Mapper(componentModel = "cdi")
+public interface ProductMapper {
+
+    @Mapping(source = "salary", target = "salary", qualifiedByName = "moneyString")
+    @Mapping(source = "expires", target = "expires", qualifiedByName = "localDateString")
+    ProductDTO toDTO(Product product);
+
+    @Mapping(source = "salary", target = "salary", qualifiedByName = "money")
+    @Mapping(source = "expires", target = "expires", qualifiedByName = "localDate")
+    Product toEntity(ProductDTO dto);
+
+    @Named("money")
+    static MonetaryAmount money(String money) {
+        if (money == null) {
+            return null;
+        }
+        return Money.parse(money);
+    }
+
+    @Named("localDate")
+    static LocalDate localDate(String date) {
+        if (date == null) {
+            return null;
+        }
+        return LocalDate.parse(date);
+    }
+
+    @Named("moneyString")
+    static String moneyString(MonetaryAmount money) {
+        if (money == null) {
+            return null;
+        }
+        return money.toString();
+    }
+
+    @Named("localDateString")
+    static String localDateString(LocalDate date) {
+        if (date == null) {
+            return null;
+        }
+        return date.toString();
+    }
+}
