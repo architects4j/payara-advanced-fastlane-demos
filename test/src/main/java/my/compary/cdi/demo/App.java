1f660946cdef0cc79de48a62abd7aaecf07474f7
Otavio Santana <otaviopolianasantana@gmail.com>
2021-10-30 19:33:39 +0100

    removes code
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- test/src/main/java/my/compary/cdi/demo/App.java
+++ test/src/main/java/my/compary/cdi/demo/App.java
@@ -1,7 +1,5 @@
 package my.compary.cdi.demo;
 
-import my.compary.cdi.demo.music.Orchestra;
-
 import javax.enterprise.inject.se.SeContainer;
 import javax.enterprise.inject.se.SeContainerInitializer;
 
@@ -9,11 +7,7 @@ public class App {
 
     public static void main(String[] args) {
         try (SeContainer container = SeContainerInitializer.newInstance().initialize()) {
-            Orchestra orchestra = container.select(Orchestra.class).get();
-            orchestra.percussion();
-            orchestra.keyboard();
-            orchestra.string();
-            orchestra.allSound();
+
 
         }
     }
