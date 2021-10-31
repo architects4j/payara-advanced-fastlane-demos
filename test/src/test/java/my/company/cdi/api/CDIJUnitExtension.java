d23bda54b0b82b1854ea5c9b854f560bd8b74f04
Otavio Santana <otaviopolianasantana@gmail.com>
2021-10-31 19:04:41 +0000

    Create CDI extension
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ test/src/test/java/my/company/cdi/api/CDIJUnitExtension.java
@@ -0,0 +1,72 @@
+package my.compary.cdi.demo.test;
+
+
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.platform.commons.util.AnnotationUtils;
+
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.inject.se.SeContainer;
+import javax.enterprise.inject.spi.AnnotatedType;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.InjectionTarget;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+class CDIJUnitExtension implements BeforeAllCallback, AfterAllCallback, BeforeEachCallback, AfterEachCallback {
+
+    private SeContainer container;
+    private CreationalContext<Object> context;
+
+    @Override
+    public void beforeAll(final ExtensionContext extensionContext) {
+        final CDIExtension config = AnnotationUtils.findAnnotation(extensionContext.getElement(), CDIExtension.class)
+                .orElse(null);
+        if (config == null) {
+            return;
+        }
+        Supplier<SeContainer> supplier = new ContainerSupplier(config);
+        container = supplier.get();
+    }
+
+    @Override
+    public void afterAll(final ExtensionContext extensionContext) {
+        if (container != null) {
+            doClose(container);
+            container = null;
+        }
+    }
+
+    @Override
+    public void beforeEach(final ExtensionContext extensionContext) {
+        if (container == null) {
+            return;
+        }
+        extensionContext.getTestInstance().ifPresent(inject());
+    }
+
+    private Consumer<Object> inject() {
+        return instance ->  {
+            final BeanManager manager = container.getBeanManager();
+            final AnnotatedType<?> annotatedType = manager.createAnnotatedType(instance.getClass());
+            final InjectionTarget injectionTarget = manager.createInjectionTarget(annotatedType);
+            context = manager.createCreationalContext(null);
+            injectionTarget.inject(instance, context);
+        };
+    }
+
+    @Override
+    public void afterEach(final ExtensionContext extensionContext) {
+        if (context != null) {
+            context.release();
+            context = null;
+        }
+    }
+
+    private void doClose(final SeContainer container) {
+        container.close();
+    }
+}
