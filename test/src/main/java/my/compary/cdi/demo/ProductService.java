5bb53366f40271eb2707543b5ac24c6535ecddcf
Otavio Santana <otaviopolianasantana@gmail.com>
2021-11-02 11:14:57 +0000

    Create Product service
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- test/src/main/java/my/compary/cdi/demo/ProductService.java
+++ test/src/main/java/my/compary/cdi/demo/ProductService.java
@@ -1,2 +1,19 @@
-package my.compary.cdi.demo;public class ProductService {
+package my.compary.cdi.demo;
+
+import javax.inject.Inject;
+
+public class ProductService {
+
+    private PaymentService service;
+
+    private ProductRepository repository;
+
+    @Inject
+    ProductService(PaymentService service, ProductRepository repository) {
+        this.service = service;
+        this.repository = repository;
+    }
+
+    public ProductService() {
+    }
 }
