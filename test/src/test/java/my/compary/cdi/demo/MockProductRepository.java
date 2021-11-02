166b11aef7aa0dbee0c216d60e73ca217ac4836f
Otavio Santana <otaviopolianasantana@gmail.com>
2021-11-02 11:26:55 +0000

    Create MockProductRepository to test propose
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ test/src/test/java/my/compary/cdi/demo/MockProductRepository.java
@@ -0,0 +1,32 @@
+package my.compary.cdi.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+public class MockProductRepository implements ProductRepository {
+
+    private final Map<Long, Product> data;
+
+    public MockProductRepository() {
+        this.data = new HashMap<>();
+    }
+
+    @Override
+    public void save(Product product) {
+        Objects.requireNonNull(product, "product is required");
+        this.data.put(product.getId(), product);
+    }
+
+    @Override
+    public Optional<Product> findById(Long id) {
+        Objects.requireNonNull(id, "id is required");
+        return Optional.ofNullable(this.data.get(id));
+    }
+
+    @Override
+    public boolean hasStock(Long id) {
+        return findById(id).isPresent();
+    }
+}
