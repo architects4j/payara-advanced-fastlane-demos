9095747940bbe27057ac180e992183ea5a775df2
Otavio Santana <otaviopolianasantana@gmail.com>
2021-11-02 11:12:11 +0000

    Add product with value
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ test/src/main/java/my/compary/cdi/demo/Product.java
@@ -0,0 +1,57 @@
+package my.compary.cdi.demo;
+
+import javax.money.MonetaryAmount;
+import java.util.Objects;
+
+public class Product {
+
+    private Long id;
+
+    private String name;
+
+    private MonetaryAmount price;
+
+    public Product(Long id, String name, MonetaryAmount price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public MonetaryAmount getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
