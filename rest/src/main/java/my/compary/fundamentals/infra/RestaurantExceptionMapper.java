e380531aa9e009c5cf6ad96779d15748e8c542d2
Otavio Santana <otaviopolianasantana@gmail.com>
2021-11-02 12:02:47 +0000

    Creates mapper to RestaurntException
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- rest/src/main/java/my/compary/fundamentals/infra/RestaurantExceptionMapper.java
+++ rest/src/main/java/my/compary/fundamentals/infra/RestaurantExceptionMapper.java
@@ -1,2 +1,34 @@
-package my.compary.fundamentals.infra;public class RestaurantExceptionMapper {
+package my.compary.fundamentals.infra;
+
+import my.compary.fundamentals.RestaurantException;
+
+import javax.validation.ConstraintViolationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class RestaurantExceptionMapper implements ExceptionMapper<RestaurantException> {
+
+    @Override
+    public Response toResponse(RestaurantException exception) {
+        return Response
+                .status(Response.Status.BAD_REQUEST)
+                .type(MediaType.APPLICATION_JSON_TYPE)
+                .entity(new Message())
+                .build();
+    }
+
+    public static class Message {
+        private final Long id;
+        private final String name;
+        private final String description;
+
+        public Message() {
+            this.id = 1L;
+            this.name = "Error name";
+            this.description = "This is a sample of error message description";
+        }
+    }
 }
