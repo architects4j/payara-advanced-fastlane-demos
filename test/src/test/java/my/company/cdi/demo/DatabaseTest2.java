d81978ff10b5b9d266165fc00d8d440f945c0a0b
Otavio Santana <otaviopolianasantana@gmail.com>
2021-11-10 11:19:04 +0000

    Create second sample with database
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- test/src/test/java/my/company/cdi/demo/DatabaseTest2.java
+++ test/src/test/java/my/company/cdi/demo/DatabaseTest2.java
@@ -2,29 +2,17 @@ package my.company.cdi.demo;
 
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@Testcontainers
-public class DatabaseTest {
+public class DatabaseTest2 {
 
-    @Container
-    private static final MySQLContainer MY_SQL_CONTAINER = new MySQLContainer();
-
-    // will be started before and stopped after each test method
-    @Container
-    private PostgreSQLContainer postgresqlContainer = new PostgreSQLContainer()
-            .withDatabaseName("foo")
-            .withUsername("foo")
-            .withPassword("secret");
 
     @Test
     void test() {
-        assertTrue(MY_SQL_CONTAINER.isRunning());
-        assertTrue(postgresqlContainer.isRunning());
+        MySQLContainer container = MySQLInstance.INSTANCE.get();
+        assertTrue(container.isRunning());
     }
 
 }
